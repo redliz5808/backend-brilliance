970c7f4dff25d1be5489292b62c4ceb03291d6b0
Elizabeth Franklin <elizabethwfranklin@gmail.com>
2025-08-01 14:55:39 -0400

    Initial Commit

---
+++ Source files/HelloRest/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+
+    console.log(`Server has started on Port ${port}`);
+
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+// Call the express server
+const express = require("express");
+
+// Call the data parsing tool
+const bodyParser = require("body-parser");
+
+// Create an app using the express server
+const app = express();
+
+// Establish a default port for the app
+const port = 3000;
+
+// Note that the URL is encoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Convert the data into JSON format
+app.use(bodyParser.json());
+
+// Tell the app what port to listen on and
+// what to do once the port is active
+app.listen(port, () => {
+    console.log(`Server has started on port ${port}`);
+});
+// Nodemon is used to monitor the server and auto-restart it.
